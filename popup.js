d7c90e5eb52ca4fdf06fcd7becb55257304c61e3
Nick Moore <nsmoore15@gmail.com>
2018-10-31 11:51:00 -0400

    POSTs are working

--- popup.js
+++ popup.js
@@ -1,77 +1,73 @@
+var all_genes = new Set();
+var all_types = new Set();
+var all_classes = new Set();
+var all_therapies = new Set();
+var all_effects = new Set();
+var all_alterations = new Set();
+var all_implications = new Set();
+var typeahead_genes = all_genes;
+
 
 // GET MOA JSON and populate selectors
 $(document).ready(function() {
-  var all_genes;
-  var all_types;
-  var all_classes;
-  var all_therapies;
-  var all_effects;
-  var all_alterations;
-  var all_implications;
-  var typeahead_genes;
 
+  var type_selector = $('#type-select');
+  var class_selector = $('#class-select');
+  var therapy_selector = $('#therapy-select');
+  var effect_selector = $('#effect-select');
+  // var alteration_selector = $('#alteration-input');
+  var implication_selector = $('#implication-select');
 
   // cleaner with .map ?
 
-  $.getJSON('http://moalmanac.org/api/assertions', function(data){
-      $.each(data, function (key, entry) {
-          if (key=='disease') {
-              all_types.append(entry);
-          }
-          if (key=='therapy_name') {
-              all_therapies.append(entry);
-          }
-          if (key=='predictive_implication') {
-              all_implications.append(entry)
-          }
-      });
+  $.getJSON('api/assertions.json', function(data) {
+          $.each(data, function (key, entry) {
+              all_types.add(entry['disease']);
+              all_therapies.add(entry['therapy_name']);
+              all_implications.add(entry['predictive_implication']);
+          });
+          populateSelector(type_selector, all_types);
+          populateSelector(therapy_selector, all_therapies);
+          populateSelector(implication_selector, all_implications);
   });
 
-  $.getJSON('http://moalmanac.org/api/alterations', function(data){
+  $.getJSON('api/alterations.json', function(data) {
       $.each(data, function (key, entry) {
-          if (key=='gene_name') {
-              all_genes.append(entry);
-          }
-          if (key=='feature') {
-              all_classes.append(entry);
-          }
-          if (key=='alt_type') {
-              all_effects.append(entry);
-          }
-          if (key=='alt') {
-              all_alterations.append(entry);
-          }
-
-      typeahead_genes = [... new Set(gene_list)];
-      var gene_list = $('#gene-input');
-
+          all_genes.add(entry['gene_name']);
+          all_classes.add(entry['feature']);
+          all_effects.add(entry['alt_type']);
+          // all_alterations.add(entry['alt']);
       });
+      populateSelector(class_selector, all_classes);
+      populateSelector(effect_selector, all_effects);
+      // populateSelector(alteration_selector, all_alterations)
   });
 
+  function populateSelector(selector, set_of_all) {
 
-  var type_list = $('#type-select');
-  var class_list = $('#class-select');
-  var therapy_list = $('#therapy-select');
-  var effect_list = $('#effect-select');
-  var alteration_list = $('#alteration-input');
-  var implication_list = $('#implication-select');
-});
+      let array_of_all = Array.from(set_of_all).sort();
+      array_of_all.forEach(function (option) {
+          if (!option == (null || "")) {
+              selector.append("<option value='" + option + "'>" + option + "</option>");
+          }
+      })
+  }
 
 
 
-// Selectors
-var implication_select = $('#implication-select')[0];
-var type_select = $('#type-select')[0]; //type of cancer
-var therapy_select = $('#therapy-select')[0];
-var class_select = $('#class-select')[0]; // e.g. rearrangement, CNV
-var effect_select = $('#effect-select')[0]; // e.g. missense, amplification
-var alteration_input = $('#alteration-input')[0];
-var source_input = $('#source-input')[0];
-var gene_input = $('#gene-input')[0];
-var email_input = $('#email-input')[0];
-
-// Reactions
 $('#btn-sub').click(function () {
+  // Selectors
+  var implication_select = $('#implication-select')[0];
+  var type_select = $('#type-select')[0]; //type of cancer
+  var therapy_select = $('#therapy-select')[0];
+  var class_select = $('#class-select')[0]; // e.g. rearrangement, CNV
+  var effect_select = $('#effect-select')[0]; // e.g. missense, amplification
+  var alteration_input = $('#alteration-input')[0];
+  var source_input = $('#source-input')[0];
+  var gene_input = $('#gene-input')[0];
+  var email_input = $('#email-input')[0];
+
+  // Reactions
   var therapy = therapy_select.value;
   var type =  type_select.value;
   var implication =  implication_select.value;
@@ -83,7 +79,7 @@ $('#btn-sub').click(function () {
   var email = email_input.value;
   $.ajax({
     type: "POST",
-    url: '',
+    url: "http://localhost:5000/submit",
     data: {
       'therapy': therapy,
       'type': type,
@@ -95,7 +91,11 @@ $('#btn-sub').click(function () {
       'alteration': alteration,
       'email': encodeURIComponent(email)
     },
+    //dataType: 'json',
+    //contentType: "application/json",
+    crossDomain: true,
     success: function (response) {
+        console.log(response);
       $('#success-panel').show();
       dict = JSON.parse(JSON.parse(response).message);
       $('#success-text').html("<b></br>Email: " + decodeURIComponent(dict.email) +
@@ -109,10 +109,13 @@ $('#btn-sub').click(function () {
                               "</br> DOI: " + dict.source + "</b>");
       $('#error-panel').hide();
     },
-    error: function (response) {
+    error: function (response, textStatus, errorThrown) {
+        console.log(response, textStatus, errorThrown);
       $('#error-panel').show();
-      $('#error-text').html(JSON.parse(response.responseText).message);
+      $('#error-text').html(JSON.parse(response.responseText));
       $('#success-panel').hide();
     }
   });
+});
+
 });
--- static/js/portal.js
+++ static/js/portal.js
@@ -1,47 +1,45 @@
 $(document).ready(function() {
-	$('[data-toggle="tooltip"]').tooltip();
-	$('.browse-dropdown').on('change', function() { this.form.submit() })
-});
 
-var substring_matcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substringRegex;
+    var substring_matcher = function (strs) {
+        return function findMatches(q, cb) {
+            var matches, substringRegex;
 
-    matches = [];
-    substrRegex = new RegExp(q, 'i');
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        matches.push(str);
-      }
-    });
+            matches = [];
+            substrRegex = new RegExp(q, 'i');
+            $.each(strs, function (i, str) {
+                if (substrRegex.test(str)) {
+                    matches.push(str);
+                }
+            });
 
-    cb(matches);
-  };
-};
+            cb(matches);
+        };
+    };
 
-if ($('.typeahead')) {
-    $('.typeahead').typeahead({
-        hint: true,
-        highlight: true,
-        minLength: 1
-    },
-    {
-        name: 'typeahead_genes',
-        source: substring_matcher(typeahead_genes)
-    });
-}
-
-$('#gene-input').bind('typeahead:select', function(ev, suggestion) {
-    $('#gene-input').value = suggestion;
-    this.form.submit();
-});
-
-$(document).ready(function(){
-    if ($('.results-table').DataTable) {
-        $('.results-table').DataTable({
-            ordering: true,
-            paging: true,
-            searching: false
-        });
+    if ($('.typeahead')) {
+        $('.typeahead').typeahead({
+                hint: true,
+                highlight: true,
+                minLength: 1
+            },
+            {
+                name: 'typeahead_genes',
+                source: substring_matcher(typeahead_genes)
+            });
     }
-});
+
+    $('#gene-input').bind('typeahead:select', function (ev, suggestion) {
+        $('#gene-input').value = suggestion;
+        this.form.submit();
+    });
+
+    $(document).ready(function () {
+        if ($('.results-table').DataTable) {
+            $('.results-table').DataTable({
+                ordering: true,
+                paging: true,
+                searching: false
+            });
+        }
+    });
+})
